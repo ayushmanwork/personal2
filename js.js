9094bf4e542fbcf3d7e62037ae2ab3c613592b52
ayushmanwork <ayushmanwork0702@gmail.com>
2026-05-19 10:49:39 +0530

    Update js.js

--- js.js
+++ js.js
@@ -1,5 +1,18 @@
-alert("READY FOR SOMETHING EXIITING");
-alert("PLZ DON'T IGNORE");
-alert("PLZ DON'T GET ANGRY");
-alert("YOUR EXAM WILL BE VERY NICE TOMMOROW");
-alert("A SMALL ......");
+alert("Do you love me OPENION AFTER CLICKING OK");
+let reply=prompt("Enter your answer yes or no").toLowerCase();
+if(reply ==="yes"){
+    alert("AI TOH VALO MEYE TUMI DEKHO  MIRROR E KOTO VALO TUMI TOMAR BF O LOVES U");
+}
+else if (reply==="no")
+{
+    while(reply!=="yes"){
+        alert("think again");
+        alert("he loves you");
+        alert("yes korte e hobe");
+        alert("korbi nh toh yes");
+        alert("oh kintu yes koraai charbe kore de kano ato website k kahnti korchis");
+        alert("kor nh..");
+        reply=prompt("Enter your answer yes or no").toLowerCase();
+        alert("AI TOH VALO MEYE TUMI DEKHO  MIRROR E KOTO VALO TUMI TOMAR BF O LOVES U");
+    }
+}
